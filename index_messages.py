0489faa8cf4e3a5988897dbf135a37536411422c
Ryan Marvin <marvinryan@ymail.com>
2025-01-27 00:45:00 +0000

    Enrich whatsapp messages with sender and time

--- index_messages.py
+++ index_messages.py
@@ -1,10 +1,13 @@
+import datetime
+
 from haystack import Pipeline, Document
 from haystack.components.writers import DocumentWriter
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
 from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack_integrations.document_stores.pgvector import PgvectorDocumentStore
 
-from whatsapp_db import get_whatsapp_messages_from_db
+from whatsapp_db import get_whatsapp_messages_from_db, WhatsappMessage
+from contacts import get_contact_name
 
 def get_store():
     document_store = PgvectorDocumentStore(
@@ -33,8 +36,18 @@ def get_whatsapp_messages_as_docs() -> list[Document]:
     messages = get_whatsapp_messages_from_db()
     docs = []
     for message in messages:
+        message = enrich_message(message)
         doc = Document(id=message["_id"], content=message["text_data"], meta={"timestamp": message["timestamp"]})
         docs.append(doc)
     return docs
 
+def enrich_message(message: WhatsappMessage) -> WhatsappMessage:
+    "Label message text with recipient name and date"
+    contact_name = get_contact_name(message["recipient"])
+    time = datetime.datetime.fromtimestamp(message["timestamp"] / 1000).strftime("%I:%M%p, %d %B %Y")
+    message["text_data"] = f"{time}, to {contact_name}: {message['text_data']}"
+    return message
+
+if __name__ == "__main__":
+    index_and_store_whatsapp_messages(get_whatsapp_messages_as_docs())
 # index_and_store_whatsapp_messages(get_whatsapp_messages_as_docs())
